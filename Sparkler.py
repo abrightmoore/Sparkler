88d5c2310842bc58b33b4e97a0dae3d7fc2e0916
Adrian Brightmoore <abrightmoore@yahoo.com.au>
2019-09-19 20:41:57 +1000

    Sparkler

---
+++ Sparkler.py
@@ -0,0 +1,201 @@
+# @TheWorldFoundry
+
+import sys
+from math import floor
+from random import randint,random
+import pygame
+from pygame.locals import *
+import time
+
+pygame.init()
+
+class Touch:
+	def __init__(self,pos,age,vx,vy,colour):
+		self.colour = colour
+		self.pos = pos
+		self.age = float(age)
+		self.alive = True
+		self.vectorx = vx
+		self.vectory = vy
+		colRange = randint(100,255)
+		# self.colour = (colRange,colRange,255,255)
+		
+	def update(self):
+		self.age -= 0.1
+		if self.age < 1:
+			self.alive = False
+		else:
+			x,y = self.pos
+			x += self.vectorx
+			y += self.vectory
+			self.pos = x,y
+		
+		
+			
+	def draw(self,surface):
+		x,y = self.pos
+		delta = int(self.age)
+		if self.alive:
+			pygame.draw.circle(surface, self.colour.getCol(),(int(x),int(y)),delta)
+			
+			#pygame.draw.lines(surface, (0,0,0,0), False, [(x,y-20),(x,y+20)], 1)
+
+class Colour:
+	paletteFlame = [
+		#(255,255,255),
+		(0,0,0),
+		(62,18,5), # Brown
+		(230,34,12),
+		(247,177,56),
+		(238,255,137),
+		(238,255,137),
+		(241,255,213),
+		(100,100,255),
+		(255,255,255)
+
+	]
+	paletteDistance = 255# Number of colour gradients in each section of the palette
+	
+	def __init__(self,palette):
+		self.gradient = 0.5+random()/2
+		self.age = 0.5+random()/2
+		self.palette = palette
+
+	def clipBounds(self,val,min,max):
+		if val < min:
+			val = min
+		if val > max:
+			val = max
+		return val
+			
+	def update(self):
+		if self.age > 0:
+			self.age -= 0.03*random()
+		else:
+			self.age = 0
+
+	def getCol(self):
+		paletteLen = self.paletteDistance*(len(self.palette)-1)
+		colPos = self.age*paletteLen
+		# if random() < 0.1: print colPos
+		baseColIdx = int(floor(colPos/self.paletteDistance))
+		baseCol = self.palette[baseColIdx]
+		nextCol = self.palette[(baseColIdx+1)%len(self.palette)] # To Do: check bounds?
+		percentDist = (colPos - self.paletteDistance*baseColIdx)/self.paletteDistance
+		R1,G1,B1 = baseCol
+		R2,G2,B2 = nextCol
+		R = R1+(R2-R1)*percentDist
+		G = G1+(G2-G1)*percentDist
+		B = B1+(B2-B1)*percentDist
+		self.update()
+		return (self.clipBounds(int(R),0,255),self.clipBounds(int(G),0,255),self.clipBounds(int(B),0,255),255)
+
+	def render(self,surface):
+		height = 32
+		width = surface.get_width()
+		paletteLen = self.paletteDistance*(len(self.palette)-1)
+		for i in xrange(0,width):
+			colPos = float(float(i)/float(width)*(float(paletteLen)))
+			baseColIdx = int(colPos/self.paletteDistance)
+			print "BCI",baseColIdx
+			baseCol = self.palette[baseColIdx]
+			nextCol = self.palette[(baseColIdx+1)%len(self.palette)] # To Do: check bounds?
+			percentDist = (float(colPos - self.paletteDistance*baseColIdx))/self.paletteDistance
+			if random() < 0.1: print percentDist
+			R1,G1,B1 = baseCol
+			R2,G2,B2 = nextCol
+			R = R1+(R2-R1)*percentDist
+			G = G1+(G2-G1)*percentDist
+			B = B1+(B2-B1)*percentDist			
+			pygame.draw.lines(surface, (self.clipBounds(int(R),0,255),self.clipBounds(int(G),0,255),self.clipBounds(int(B),0,255),255), False, [(i,0),(i,height)], 1)
+
+
+class Game:
+	def __init__(self,size,label,FPS):
+		self.size = size
+		self.label = label
+		self.FPS = FPS
+		self.FTICKS = int(100/FPS)
+
+	def setupScreen(self):		
+		print "Creating Surface and Window"
+		surface = pygame.display.set_mode(self.size,SRCALPHA)
+		print "Converting the surface to optimise rendering"
+		surface.convert()
+		print "Changing the caption"
+		pygame.display.set_caption(self.label)
+		fpsClock = pygame.time.Clock()
+		fpsClock.tick(self.FPS)
+		return surface
+
+	def addSparkle(self,objects,event):
+		x,y = event.pos
+		sz = 20
+		objects.append(Touch(event.pos,randint(5,sz>>1),0,randint(0,6)*0.13,Colour(Colour.paletteFlame)))
+		for i in xrange(0,randint(1,23)):
+				deltax = randint(-sz,sz)
+				deltay = randint(-sz,sz)
+				objects.append(Touch((x+deltax,y+deltay),randint(2,5),randint(-3,3)*0.5,randint(0,2)*0.5,Colour(Colour.paletteFlame)))	
+		return objects
+
+	def gameLoop(self):
+		WIDTH,HEIGHT = self.size
+		cx = WIDTH>>1
+		cy = HEIGHT>>1
+		
+		objects = []
+		
+		# Setup objects
+		
+		surface = self.setupScreen()    
+		iterationCount = 0
+		keepGoing = True
+		
+		current_milli_time = pygame.time.get_ticks() #int(round(time.time() * 1000)) # Seed time tracker
+		
+		#COL = Colour()
+
+		
+		while keepGoing:
+			iterationCount += 1
+			for event in pygame.event.get():
+				if event.type == pygame.QUIT:
+					keepGoing = False
+				elif event.type == MOUSEBUTTONUP:
+					if event.button == 1: # 1 == Left
+						print "Left mouse click"
+						print event
+						objects.append(Touch(event.pos,80,randint(-2,2),1,Colour()))
+				elif event.type == MOUSEMOTION:
+					if random() <= 0.8:
+						objects = self.addSparkle(objects,event)
+				else:
+					print event
+
+			surface.fill((0,40,60))
+			newObjects = [] # This is a swap buffer
+			for o in objects:
+				o.update()
+				if o.alive == True:
+					o.draw(surface)
+					newObjects.append(o)
+			#COL.render(surface)					
+			pygame.display.update()
+			o = newObjects
+			
+			current_milli_time2 = pygame.time.get_ticks() #int(round(time.time() * 1000))
+			timeDelta = current_milli_time2-current_milli_time
+			#print timeDelta
+			if timeDelta > 1000: print "System is running at or below 1FPS."
+			elif timeDelta < self.FTICKS:
+				pygame.time.wait(self.FTICKS-timeDelta)
+			
+			current_milli_time = current_milli_time2
+
+		print "Shutting down."
+		pygame.quit()
+		sys.exit()	
+
+g = Game((600,600),"Game Name",10)
+g.gameLoop()
+
